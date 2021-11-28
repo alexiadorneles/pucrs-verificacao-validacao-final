37a995b64dddb73c1788f644ea6e42311914cac5
Aléxia Dorneles <alexiadorneles02@gmail.com>
2021-11-27 22:59:39 -0300

    test: add unittests for CustoViagem

---
+++ src/test/java/edu/pucrs/casosDeUso/politicas/CustoViagemTest.java
@@ -0,0 +1,27 @@
+package edu.pucrs.casosDeUso.politicas;
+
+import edu.pucrs.entidades.Passageiro;
+import edu.pucrs.entidades.Roteiro;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CustoViagemTest {
+
+    @Test
+    void custoViagem() {
+        ICalculoCustoViagem calculadora = mock(ICalculoCustoViagem.class);
+        double esperado = 99d;
+        when(calculadora.custoViagem()).thenReturn(esperado);
+        CustoViagem custoViagem = new CustoViagem(calculadora);
+        Roteiro roteiro = mock(Roteiro.class);
+        Passageiro passageiro = mock(Passageiro.class);
+
+        double resultado = custoViagem.custoViagem(roteiro, passageiro);
+        assertEquals(esperado, resultado);
+        verify(calculadora, times(1)).custoViagem();
+        verify(calculadora, times(1)).definePassageiro(passageiro);
+        verify(calculadora, times(1)).defineRoteiro(roteiro);
+    }
+}
